21e9927e2b8946d6aeeadfb54f3da42896159380
iGorBuck <igogolev74@yandex.ru>
2019-07-03 19:47:44 +0500

    first commit

---
+++ src/components/pages/index.js
@@ -0,0 +1,13 @@
+import PeoplePage from './people-page';
+import PlanetPage from './planet-page';
+import StarshipsPage from './starships-page';
+import SecretPage from './secret-page';
+import LoginPage from './login-page';
+
+export {
+    PeoplePage,
+    PlanetPage,
+    StarshipsPage,
+    SecretPage,
+    LoginPage
+};
